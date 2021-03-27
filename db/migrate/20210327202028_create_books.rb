09e41a1f509cc02cf6bc287b10a909aae4331d73
tomas agnese <tagnese1985@gmail.com>
2021-03-27 17:24:16 -0300

    book model created

---
+++ db/migrate/20210327202028_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.integer :rating
+      t.string :library
+      t.integer :price
+      t.boolean :fav
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
