5217a9dbcbea5a2b0533d81cd0bbb7d4ed1dd729
rocio picaza <rocio.picaza@gmail.com>
2021-04-03 12:26:37 -0300

    change tittle datatype to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_204054) do
+ActiveRecord::Schema.define(version: 2021_04_03_152238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.string "title"
+    t.text "title"
     t.text "description"
     t.integer "rating"
     t.string "library"
